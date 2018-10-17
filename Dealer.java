be94d0309d54443480b64891962395fe45b03fdc
Mohammad Raunak <raunak@loyola.edu>
2018-10-17 11:50:11 -0400

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,5 +1,6 @@
 package com.example.christina.simpleblackjack;
-/** An Object class to represent the User of the BlackJack game.
+/** 
+ * An Object class to represent the User of the BlackJack game.
  *
  * @author Christina Russo
  * @version 1.0 10/6/18
@@ -8,12 +9,14 @@ package com.example.christina.simpleblackjack;
  */
 public class Dealer extends Player {
 
-    /**The Default constructor sets the max amount of moves possible and sets the Dealer's initial choice for hitting to be false.
-     *
+    /**
+     * The Default constructor sets the max amount of moves possible and sets the Dealer's initial choice for hitting to be false.
      */
     public Dealer(){
         super.movesLeft=5;
         super.hitChoice = false;
     }
-
+    // Since Dealer does not have any specialized behavior, for this application, making the dealer just an object created out 
+    // of the super class Player and having Player as a concrete class would have been fine. 
+    
 }
